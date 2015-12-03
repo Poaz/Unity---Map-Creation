19338f35b445f7fe1fc8a0f589aae9a92b187e5e
Dines Selvig <Dines@rxdesign.dk>
2015-12-03 14:11:48 +0100

    Start

---
+++ Assets/Scripts/Desert.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+//[RequireComponent(typeof(BoxCollider))]
+public class Desert : Singleton<Desert> {
+	
+//private Texture whatevercolor = Resources.Load("grassland2.jpg") as Texture;
+ 
+public void Start()
+{
+        //yield return new WaitForSeconds(3f);
+
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+        //MeshCollider[] meshColliders = GetComponentsInChildren<MeshCollider>();
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+       // CombineInstance[] combinecolider = new CombineInstance[meshColliders.Length];
+        int i = 0;
+        while (i < meshFilters.Length) {
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            meshFilters[i].gameObject.SetActive(true);
+            i++;
+        }
+
+        Texture mat = Resources.Load("desert") as Texture;
+        //renderer.material = mat;
+        transform.GetComponent<MeshFilter>().mesh = new Mesh();
+        transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
+        gameObject.GetComponent<Renderer>().material.mainTexture = mat;
+        transform.gameObject.AddComponent<BoxCollider>();
+        transform.GetComponent<MeshFilter>().mesh.Optimize();
+        
+        transform.gameObject.SetActive(true);
+        this.gameObject.isStatic = true;
+        
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : Singleton<Movement>
+{
+    public float speed;
+
+    // Use this for initialization
+    private void Start()
+    {
+        print("Starting up...");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float move_Horizontal = Input.GetAxis("Horizontal");
+        float move_Vertical = Input.GetAxis("Vertical");
+       
+        Vector3 movement = new Vector3(move_Horizontal, 0.0f, move_Vertical);
+        movement = Camera.main.transform.TransformDirection(movement);
+
+        GetComponent<Rigidbody>().velocity = movement*speed;
+
+    }
+}   
